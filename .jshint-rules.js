8451b1196ef291496206698307fd6d34a565e737
Randy Hunt <letsgetrandy@gmail.com>
2016-02-06 20:50:32 -0600

    loosen Hound messages

---
+++ .jshint-rules.js
@@ -0,0 +1,32 @@
+{
+  "asi": false,
+  "bitwise": true,
+  "browser": true,
+  "camelcase": true,
+  "curly": true,
+  "forin": true,
+  "immed": true,
+  "latedef": "nofunc",
+  "maxlen": 80,
+  "newcap": true,
+  "noarg": true,
+  "noempty": true,
+  "nonew": false,
+  "predef": [
+    "$",
+    "jQuery",
+
+    "jasmine",
+    "beforeEach",
+    "describe",
+    "expect",
+    "it",
+
+    "angular",
+    "inject",
+    "module"
+  ],
+  "trailing": true,
+  "undef": true,
+  "unused": true
+}
